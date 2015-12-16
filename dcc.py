5f662d349a5a60b8279c8559ea20bb1de1fe4fdb
heli <ohhlyhly@gmail.com>
2015-12-16 09:28:46 +0800

    test activation

--- dcc.py
+++ dcc.py
@@ -9,27 +9,30 @@ from keras.optimizers import SGD
 
 from data import load_data, shift
 
+conAct = 'tanh'
+denAct = 'sigmoid'
+
 def build_cnn(window_size=95):
     model = Sequential()
     
     model.add(Convolution2D(48, 4, 4, input_shape=(1, window_size, window_size)))
-    model.add(Activation('tanh'))
+    model.add(Activation(conAct))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Convolution2D(48, 5, 5))
-    model.add(Activation('tanh'))
+    model.add(Activation(conAct))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Convolution2D(48, 4, 4))
-    model.add(Activation('tanh'))
+    model.add(Activation(conAct))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Convolution2D(48, 4, 4))
-    model.add(Activation('tanh'))
+    model.add(Activation(conAct))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())
     model.add(Dense(200))
-    model.add(Activation('sigmoid'))
+    model.add(Activation(denAct))
     model.add(Dense(2))
-    model.add(Activation('sigmoid'))
+    model.add(Activation(denAct))
 
     sgd = SGD(lr=0.1, nesterov=False)
     model.compile(loss='categorical_crossentropy', optimizer=sgd)
