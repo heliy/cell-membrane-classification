923af128358673ae95190c65e808667f17dd1e01
heli <ohhlyhly@gmail.com>
2015-12-16 11:35:40 +0800

    dropout

--- dcc.py
+++ dcc.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten
+from keras.layers.core import Dense, Activation, Flatten, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
 
