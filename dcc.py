454c75c8bbabf527e8ceba8d8c2d00a5c6ecfdcf
heli <ohhlyhly@gmail.com>
2015-12-16 11:35:24 +0800

    dropout

--- dcc.py
+++ dcc.py
@@ -12,33 +12,44 @@ from data import load_data, shift
 conAct = 'tanh'
 denAct = 'sigmoid'
 
+lr = 0.1
+
+nesterov = False
+dropout = 0
+
+
 def build_cnn(window_size=95):
     model = Sequential()
     
     model.add(Convolution2D(48, 4, 4, input_shape=(1, window_size, window_size)))
     model.add(Activation(conAct))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(dropout))
     model.add(Convolution2D(48, 5, 5))
     model.add(Activation(conAct))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(dropout))
     model.add(Convolution2D(48, 4, 4))
     model.add(Activation(conAct))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(dropout))
     model.add(Convolution2D(48, 4, 4))
     model.add(Activation(conAct))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(dropout))
 
     model.add(Flatten())
     model.add(Dense(200))
+    model.add(Dropout(dropout))
     model.add(Activation(denAct))
     model.add(Dense(2))
     model.add(Activation(denAct))
 
-    sgd = SGD(lr=0.1, nesterov=False)
+    sgd = SGD(lr=lr, nesterov=nesterov)
     model.compile(loss='categorical_crossentropy', optimizer=sgd)
     return model
 
-def train_model(model, num=2000, times=10, epoch=30):
+def train_model(model, num=3000, times=10, epoch=30):
     for i in range(times):
         print(i, ":")
         (tx, ty, vx, vy) = load_data(positiveNum=num//2, negativeNum=num//2)
