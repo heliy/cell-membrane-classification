67b3ae8d9903540180a2058ba971faa716e245db
heli <ohhlyhly@gmail.com>
2015-12-23 16:32:23 +0800

    predict fix

--- post.py
+++ post.py
@@ -9,14 +9,14 @@ import numpy as np
 
 import caffe
 
-def load_model(model_dir, window_size=65, use_GPU=True):
+def load_net(model_dir, window_size=65, use_GPU=True):
     model_file = list(filter(lambda i: '.caffemodel' in i, os.listdir(model_dir)))[0]
     use_GPU and caffe.set_mode_gpu() or caffe.set_mode_cpu()
     net = caffe.Classifier(os.path.join(model_dir, 'deploy.prototxt'),
                            os.path.join(model_dir, model_file),
                            image_dims=(window_size, window_size),
                            input_scale=1, raw_scale=255)
-    return model
+    return net
 
 def tolist(data):
     for x in data:
