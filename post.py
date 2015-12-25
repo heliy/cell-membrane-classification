3f63dd2a4e8bfb805f45f41c6d5892489e02025f
heli <ohhlyhly@gmail.com>
2015-12-25 14:18:00 +0800

    leastsq

--- post.py
+++ post.py
@@ -6,6 +6,7 @@
 import os
 
 import numpy as np
+from scipy.optimize import leastsq
 
 import caffe
 
@@ -22,14 +23,30 @@ def gen(data):
     for x in data:
         yield x
 
+def __predict(net, npyfile):
+    X = np.load(npyfile)
+    print npyfile, X.shape[0]
+    window_size = X.shape[1]
+    X = X.reshape((X.shape[0], window_size, window_size, 1))
+    return net.predict(list(gen(X)))
+
+
 def predict(net, npy_files):
-    ys = []
-    for npyfile in npy_files:
-        X = np.load(npyfile)
-        print npyfile, X.shape[0]
-        window_size = X.shape[1]
-        X = X.reshape((X.shape[0], window_size, window_size, 1))
-        ys.append(net.predict(list(gen(X))))
-    ys = np.array(ys)
-    return ys.reshape((ys.size//2, 2))
+    batch_num = len(npy_files)//batch_size+1
+    for i in npy_files:
+        np.save(i.replace(".npy", "_result.npy"), __predict(net, i))
+    
+def leastsq_fit(Y, X):
+    def residuals(p, y, x):
+        p3, p2, p1, p0 = p
+        err = y - (p3*(x**3)+p2*(x**2)+p1*x+p0)
+        return err
+    p0 = [0.001, 0.001, 1, 0.0001]
+    plsq = leastsq(residuals, p0, args=(Y, X))
+
+    def eva(x):
+        p = plsq[0]
+        return p[3]*(x**3)+p2*(x**2)+p1*x+p0
+
+    return eva
 
