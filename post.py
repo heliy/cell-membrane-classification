187277b5c7d95c7aa0b2dc575cf102cf1c918855
heli <ohhlyhly@gmail.com>
2015-12-26 13:39:30 +0800

    predict batch

--- post.py
+++ post.py
@@ -10,13 +10,15 @@ from scipy.optimize import leastsq
 
 import caffe
 
-def load_net(model_dir, window_size=65, use_GPU=True):
+def load_net(model_dir, window_size=65, gpu_id=True):
     model_file = list(filter(lambda i: '.caffemodel' in i, os.listdir(model_dir)))[0]
-    use_GPU and caffe.set_mode_gpu() or caffe.set_mode_cpu()
-    net = caffe.Classifier(os.path.join(model_dir, 'deploy.prototxt'),
-                           os.path.join(model_dir, model_file),
-                           image_dims=(window_size, window_size),
-                           input_scale=1, raw_scale=255)
+    if gpu_id is None:
+        caffe.set_mode_gpu()
+        caffe.setdevice(gpu_id)
+    else:
+        caffe.set_mode_cpu()
+    net = caffe.Net(os.path.join(model_dir, 'deploy.prototxt'), os.path.join(model_dir, model_file),
+                    caffe.TEST)
     return net
 
 def gen(data):
@@ -36,6 +38,30 @@ def predict(prefix, net, npy_files):
     for f in npy_files:
         print("%d / %d" % (i, len(npy_files)))
         np.save(f.replace(".npy", "_result_"+prefix+".npy"), __predict(net, f))
+
+batch_size = 64
+        
+def batch_predict(postfix, net, npy_files):
+    files_total = len(npy_files)
+    for (file_no, f) in enumerate(npy_files):
+        print("file: %s, %d / %d" % (f, file_no, files_total))
+        X = np.load(f)
+        window = X.shape[1]
+        Y = np.zeros((X.shape[0], 2))
+        if X.shape[0] % batch_size == 0:
+            batch_num = int(X.shape[0]/batch_size)
+        else:
+            batch_num = int(X.shape[0]/batch_size)+1
+        for batch_no in range(batch_num):
+            print("batch: %d / %d" % (batch_no, batch_num))
+            batch = X[batch_no*batch_size:(batch_no+1)*batch_size, :, :]
+            batch = batch.reshape((batch_size, 1, window, window))
+            net.blobs['data'].data[...] = batch
+            Y[batch_no*batch_size:(batch_no+1)*batch_size] = net.forward()
+        name = f.replace(".npy", "_result_"+postfix+".npy")
+        print("save in %s ..." % (name))
+        np.save(name, Y)
+        
     
 def leastsq_fit(X, Y):
     def residuals(p, y, x):
