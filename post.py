6ce72f311cfa6c21c4a0316441596b55b8d386e1
heli <ohhlyhly@gmail.com>
2015-12-27 17:16:32 +0800

    prob eval

--- post.py
+++ post.py
@@ -103,7 +103,7 @@ def prob_eval(net_dir='models/n1', xfiles, yfiles, y_index=1, gpu_id=0):
 
     X = prob_dict.keys()
     Y = np.array([float(prob_dict[x][1])/sum(prob_dict[x]) for x in X])
-    reutrn leastsq_fit(X, Y)
+    return leastsq_fit(X, Y)
 
 def threshold_filter(narray, threshold=0.01):
     '''if the value in narray < threshold, it will be setted as threshold'''
