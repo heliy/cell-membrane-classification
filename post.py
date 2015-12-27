c8505b2db15dd1234c7bd451c64a3b12bb865b44
heli <ohhlyhly@gmail.com>
2015-12-27 17:15:50 +0800

    prob eval

--- post.py
+++ post.py
@@ -67,12 +67,11 @@ def save_predict(postfix, net, npy_files):
         np.save(name, Y)
         
     
-def leastsq_fit(X, Y):
+def leastsq_fit(X, Y, p0=[0.01, 0.01, 0.01, 0.01]):
     def residuals(p, y, x):
         p3, p2, p1, p0 = p
         err = y - (p3*(x**3)+p2*(x**2)+p1*x+p0)
         return err
-    p0 = [0.001, 0.001, 1, 0.0001]
     plsq = leastsq(residuals, p0, args=(Y, X))
 
     def eva(x):
@@ -81,9 +80,35 @@ def leastsq_fit(X, Y):
 
     return eva
 
+def prob_eval(net_dir='models/n1', xfiles, yfiles, y_index=1, gpu_id=0):
+    '''
+    y_index = 1 for n1/n2, = 0 for n3/n4
+       as when we train the net, the first col in prob is different Orz.
+    '''
+    prob_dict = {}
+    net = load_net(net_dir, gpu_id)
+
+    @np.vectorize
+    def input_to_dict(predicted_y, real_y):
+        predict = int(predicted_y*10000)/10000.
+        if predict not in prob_dict:
+            prob_dict[predict] = [0, 0]
+        prob_dict[predict][real_y] += 1
+    
+    for (x, y) in zip(xfiles, yfiles):
+        X = np.load(x)
+        Y = np.load(y)
+        y = batch_predict(net, X)
+        input_to_dict(y[:, y_index], Y[:, 0])
+
+    X = prob_dict.keys()
+    Y = np.array([float(prob_dict[x][1])/sum(prob_dict[x]) for x in X])
+    reutrn leastsq_fit(X, Y)
+
 def threshold_filter(narray, threshold=0.01):
     '''if the value in narray < threshold, it will be setted as threshold'''
     idx = narray < threshold
     narray[idx] = threshold
     return narray
 
+def 
